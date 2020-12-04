04b90e1b414608bf107b8824d51b42f86876c465
BEAGLE3 <71073090+BEAGLE3@users.noreply.github.com>
2020-12-04 09:37:29 +0530

    Add files via upload

---
+++ Plinko.js
@@ -0,0 +1,33 @@
+class Plinko {
+constructor(x, y ,r) {
+var options = {
+isStatic:true,
+restitution: 1,
+friction: 0,
+density: 0.04
+}
+this.y = y
+this.x = x
+this.r = r
+this.r = 10;
+this.body = Bodies.circle(x, y, this.r, options);
+        
+World.add(world, this.body);
+}
+display() {
+
+var pos = this.body.position;
+var angle = this.body.angle;
+
+push();
+translate(pos.x, pos.y);
+rotate(angle);
+imageMode(CENTER);
+noStroke();
+fill("white");
+ellipseMode(RADIUS);
+ellipse(0,0,this.r,this.r);
+pop();
+}
+
+};
